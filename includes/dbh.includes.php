e4271bfcba7c3ea60b5fc21a1e4cca60726ae0b7
Michael Procyk <mprocyk022304@gmail.com>
2024-10-13 11:03:55 -0400

    updating database to connect to website

--- includes/dbh.includes.php
+++ includes/dbh.includes.php
@@ -1,9 +1,9 @@
 <?php
 
-$severName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "studybuddy";
+$severName = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dBUsername = "wevq715pk0aczspm";
+$dBPassword = "ljuyiscekm3jh9d2";
+$dBName = "m68su2qjsgrlqvu2";
 
 $conn = mysqli_connect($severName, $dBUsername, $dBPassword, $dBName);
 
